edb5be047d6fccf6823e326abd5cc8adde47d500
Wanda-Jih <43124480+Wanda-Jih@users.noreply.github.com>
2019-11-04 23:24:20 +0800

    Update linear regression.py

--- linear regression.py
+++ linear regression.py
@@ -47,27 +47,10 @@ test=pd.DataFrame(scaler.fit_transform(test),columns=test.columns,index=test.ind
 
 
 #logistic regression
-from sklearn.feature_selection import RFE
+#from sklearn.feature_selection import RFE
 from sklearn.linear_model import LogisticRegression
 
-'''
-LogisticRegression(
-        penalty='l2',
-        dual=False,
-        tol=1e-4,
-        C=1.0,
-        fit_intercept=True,
-        intercept_scaling=1,
-        class_weight=None,
-        random_state=None,
-        solver='liblinear',
-        max_iter=100,
-        multi_class= 'ovr',
-        verbose=0,
-        warm_start=False, 
-        n_jobs=1)
 
-'''
 log = LogisticRegression(
         penalty='l1', #l1:拉普拉斯分佈; l2:高斯分佈
         dual=False,
@@ -90,7 +73,6 @@ log.fit(x_train,y_train)
 
 #result
 prediction = log.predict(test)
-#print(prediction)
 prediction = pd.DataFrame(prediction)
 prediction.index += 250
 prediction.columns = ['target']
